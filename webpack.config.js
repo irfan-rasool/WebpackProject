ac00a26c0b12a97f91137e4c91ec0bed26e68e36
Irfan Rasool <irfan@web-flight.nl>
2017-09-18 15:30:17 +0530

    #comment define production env

--- webpack.config.js
+++ webpack.config.js
@@ -34,6 +34,9 @@ module.exports = {
     }),
     new HTMLWebpackPlugin({
       template: 'src/index.html'
-    })
+    }),
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify('process.env.NODE_ENV')
+    }),
   ]
 };
